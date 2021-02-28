2f26d5b41ec7a0f1f129bf2ebb8614f616a9832e
surendrak800 <79745761+surendrak800@users.noreply.github.com>
2021-02-28 22:06:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Searchcountry from './Components/Searchcountry';
+
+
+function App() {
+  return (
+    <div >
+     <Searchcountry/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Searchcountry.js
@@ -0,0 +1,57 @@
+import React from "react";
+import '../App.css';
+import { useState, useEffect} from "react";
+import countries from "./Countryname";
+
+ const Searchcountry = () => {
+    const [ searchItem, setSearchItem] = useState("");
+    const [countryData] = useState(countries);
+    const [ searchedcountries, setSearchedcountries] = useState([]);
+
+const handlechange = (e) =>{
+    e.preventDefault();
+    setSearchItem(e.target.value);
+};
+
+useEffect(() =>{
+    const delayFn = setTimeout(() => {
+        const data = 
+        searchItem !== ""
+        ? countryData.filter((d) =>
+        d.toLocaleLowerCase().includes(searchItem.toLocaleLowerCase())    
+        )
+        : [];
+        setSearchedcountries(data);
+
+        
+    }, 1000);
+
+    
+    return() => clearTimeout(delayFn);
+},[searchItem]);
+
+return(
+    <>
+    <div className="content">
+        <div className="inputData">
+            <input type="search"
+            placeholder="Searching..."
+            onChange={handlechange}
+            value={searchItem}
+            />
+            {searchedcountries.map((d,i) => (
+                <li key={i}>{d}</li>
+            ))}
+        </div>
+    </div>
+    </>
+
+);
+
+
+
+
+
+ };
+
+ export default Searchcountry;
